9bdeaa5538294a56d5a622559511a3fd86c1d138
riku2005 <149132633+riku2005@users.noreply.github.com>
2024-06-21 23:26:18 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+
+class norimono
+{
+public:
+	? ？ ? void Voice() {
+		printf("あにまる¥n");
+	};
+};
+
+class kuruma : public norimono
+{
+public:
+	void Voice() {
+		printf("kurumaっす\n");
+	};
+};
+
+class hikouki : public norimono
+{
+public:
+	void Voice() {
+		printf("hikoukiっす\n");
+	};
+};
+
+int main(void) {
+
+	norimono* 配列名[2] = { new kuruma,new hikouki };
+
+	animal[0]->Voice();
+	animal[1]->Voice();
+
+	delete 配列名[0];
+	delete 配列名[1];
+
+	return 0;
+}
